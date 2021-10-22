7ab25832c6e954606e242a244f1f29b0f6b4efd8
John Dillick <john@dillick.us>
2021-10-22 18:55:26 -0400

    Update mock playlists.

--- docs/api_project.js
+++ docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-10-20T14:08:45.333Z",
+    "time": "2021-10-22T22:51:27.314Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
